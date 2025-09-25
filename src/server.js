2e4655721b1cdea7da596973324c5405ae64dea3
LS <ludddka@gmail.com>
2025-09-25 20:22:05 +0200

    error

--- src/server.js
+++ src/server.js
@@ -33,10 +33,6 @@ app.get('/notes/:noteId', (req, res) => {
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
-
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
