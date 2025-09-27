58ab829adb7dbc567064602fdaa58d3ac306e94b
LS <ludddka@gmail.com>
2025-09-27 14:12:27 +0200

    fix2

--- src/server.js
+++ src/server.js
@@ -33,13 +33,14 @@ app.get('/notes/:noteId', (req, res) => {
   res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
 });
 
+app.get('/test-error', (req, res) => {
+  throw new Error('Something went wrong');
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
-app.get('/test-error', (req, res) => {
-  throw new Error('Something went wrong');
-});
 
 app.use((err, req, res, next) => {
   console.error('Error:', err.message);
