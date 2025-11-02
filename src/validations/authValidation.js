7f34a82fdad37f7823c5d1a9cf08cdccdcfa84f8
LS <ludddka@gmail.com>
2025-11-02 10:24:30 +0100

    fix

--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -17,9 +17,10 @@ export const requestResetEmailSchema = {
     email: Joi.string().email().required(),
   }),
 };
+
 export const resetPasswordSchema = {
   [Segments.BODY]: Joi.object({
-    password: Joi.string().required(),
+    password: Joi.string().min(8).required(),
     token: Joi.string().required(),
   }),
 };
